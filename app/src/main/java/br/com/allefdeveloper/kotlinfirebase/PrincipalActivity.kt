304dc2fd56f04bddd8b294addcbdf06493950bd9
Allef Sousa <allefsousa_1@hotmail.com>
2018-01-18 00:04:35 -0200

    Login anonimo adicionado

--- app/src/main/java/br/com/allefdeveloper/kotlinfirebase/PrincipalActivity.kt
+++ app/src/main/java/br/com/allefdeveloper/kotlinfirebase/PrincipalActivity.kt
@@ -2,11 +2,22 @@ package br.com.allefdeveloper.kotlinfirebase
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 
 class PrincipalActivity : AppCompatActivity() {
+    // instancia de autenticacao do firebase
+     var firebaseAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_principal)
+
+
+        loginAnonimo();
+    }
+
+    private fun loginAnonimo() {
+        firebaseAuth.signInAnonymously()
+
     }
 }
