3ea4ffcc884c1df21c2f6220f02372ce407bfa7f
Allef Sousa <allefsousa_1@hotmail.com>
2018-02-06 23:18:06 -0200

    Estrutura do projeto atualizada. Pacotes, claases

--- app/src/main/java/br/com/allefdeveloper/kotlinfirebase/View/PrincipalActivity.kt
+++ app/src/main/java/br/com/allefdeveloper/kotlinfirebase/View/PrincipalActivity.kt
@@ -1,23 +1,27 @@
-package br.com.allefdeveloper.kotlinfirebase
+package br.com.allefdeveloper.kotlinfirebase.View
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import br.com.allefdeveloper.kotlinfirebase.R
 import com.google.firebase.auth.FirebaseAuth
 
 class PrincipalActivity : AppCompatActivity() {
     // instancia de autenticacao do firebase
      var firebaseAuth = FirebaseAuth.getInstance()
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_principal)
 
 
-        loginAnonimo();
+        loginAnonimo()
     }
 
+    /**
+     * metodo responsavel pelo login anonymo do firebase
+     */
     private fun loginAnonimo() {
         firebaseAuth.signInAnonymously()
-
     }
 }
