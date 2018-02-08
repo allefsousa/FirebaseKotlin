fc548b23a2d11c4516044f02c1180d8ff9081598
Allef Sousa <allefsousa_1@hotmail.com>
2018-02-07 23:57:54 -0200

    Salvando dados no Realtime database do Firebase

--- app/src/main/java/br/com/allefdeveloper/kotlinfirebase/View/PrincipalActivity.kt
+++ app/src/main/java/br/com/allefdeveloper/kotlinfirebase/View/PrincipalActivity.kt
@@ -2,26 +2,52 @@ package br.com.allefdeveloper.kotlinfirebase.View
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import br.com.allefdeveloper.kotlinfirebase.Model.Projetos
 import br.com.allefdeveloper.kotlinfirebase.R
+import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.Logger
 
 class PrincipalActivity : AppCompatActivity() {
     // instancia de autenticacao do firebase
-     var firebaseAuth = FirebaseAuth.getInstance()
-
+     var firebaseAuth :FirebaseAuth? = null
+    var databaseReference: DatabaseReference? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_principal)
-
-
+        initFirebase()
         loginAnonimo()
+
+
+
+       val projeto = Projetos("Projeto comercial","","","Java, Mobile, Android")
+        salvarProjeto(projeto)
+
+    }
+
+    private fun salvarProjeto(projetos: Any) {
+        databaseReference?.child("projetos")?.setValue(projetos)
     }
 
     /**
      * metodo responsavel pelo login anonymo do firebase
      */
     private fun loginAnonimo() {
-        firebaseAuth.signInAnonymously()
+        firebaseAuth?.signInAnonymously()
+
+    }
+
+    /**
+     * Metodo responsavel por inicializar o firebase na aplicação
+     */
+    private fun initFirebase(){
+        FirebaseApp.initializeApp(applicationContext)
+        firebaseAuth = FirebaseAuth.getInstance()
+        FirebaseDatabase.getInstance().setLogLevel(Logger.Level.DEBUG)
+        //get reference to our db
+        databaseReference = FirebaseDatabase.getInstance().reference
     }
 }
